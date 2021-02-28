f32426a4bc504aa4425b1e344db08e65713f81f8
施佑學 <yoshi@biboting.com>
2021-02-28 22:15:25 +0800

    show received binary data

--- src/main.cpp
+++ src/main.cpp
@@ -13,30 +13,35 @@ esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
     switch (event->event_id) {
         case MQTT_EVENT_CONNECTED://MQTT连上事件
             Serial.printf("MQTT_EVENT_CONNECTED\n");
-            msg_id = esp_mqtt_client_subscribe(client, "/topic/iot_monitor", 1);
+            msg_id = esp_mqtt_client_subscribe(client, "/iot_monitor/device", 1);
             // ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
             break;
         case MQTT_EVENT_DISCONNECTED://MQTT断开连接事件
+            Serial.printf("MQTT_EVENT_DISCONNECTED\n");
             // ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
             break;
         case MQTT_EVENT_SUBSCRIBED://MQTT发送订阅事件
-            // ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-            msg_id = esp_mqtt_client_publish(client, "/topic/iot_monitor", "订阅成功", 0, 0, 0);
+            Serial.printf("MQTT_EVENT_SUBSCRIBED, msg_id=%d\n", event->msg_id);
             // ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
             break;
         case MQTT_EVENT_UNSUBSCRIBED://MQTT取消订阅事件
             // ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
             break;
         case MQTT_EVENT_PUBLISHED://MQTT发布事件
-            // ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+            Serial.printf("MQTT_EVENT_PUBLISHED, msg_id=%d\n", event->msg_id);
             break;
         case MQTT_EVENT_DATA://MQTT接受数据事件
-            // ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+            Serial.printf("MQTT_EVENT_DATA\n");
+
             printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);   //主题
-            printf("DATA=%.*s\r\n", event->data_len, event->data);      //内容
+            // printf("DATA=%.*s\r\n", event->data_len, ss);      //内容
+            for (int i = 0; i < event->data_len; i++)
+                printf("%02x ", event->data[i]);
+            printf("\n");
+
             break;
         case MQTT_EVENT_ERROR://MQTT错误事件
-            Serial.printf("MQTT_EVENT_ERROR");
+            Serial.printf("MQTT_EVENT_ERROR\n");
             break;
     }
     return ESP_OK;
@@ -56,7 +61,7 @@ void mqtt_app_start(void)
 
 void setup() {
     Serial.begin(115200);
-    WiFi.begin("dodo", "89326303");
+    WiFi.begin("xx", "xxx");
     Serial.printf("WIFI_CONNECTING\n");
     while (WiFi.status() != WL_CONNECTED) //等待网络连接成功
     {
