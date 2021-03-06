5a29fb6d572ecd44400ec105fe24529d948eaa33
施佑學 <yoshi@biboting.com>
2021-03-06 11:12:08 +0800

    implement motor command

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,38 @@
 extern const uint8_t mosquitto_org_pem_start[]   asm("_binary_src_mosquitto_org_pem_start");
 extern const uint8_t mosquitto_org_pem_end[]   asm("_binary_src_mosquitto_org_pem_end");
 
+const char *topic_rx = "/units/1111/rx";
+const char *topic_tx = "/units/1111/tx";
+
+esp_mqtt_client_handle_t client;
+
+void publishMotorSetting() {
+    printf("Publish Motor Setting\n");
+    char data[] = {0x02, 0x04, 0x01, 0x01, 0x23, 0x00, 0x10, 0x22, 0x33};
+    esp_mqtt_client_publish(client, topic_tx, data, 9, 1, 0);
+}
+
+void publishMotorStatus() {
+    printf("Publish Motor Status\n");
+    char data[] = {0x02, 0x05, 0x00};
+    esp_mqtt_client_publish(client, topic_tx, data, 3, 1, 0);
+}
+
+void parseMqttData(char *data, int len) {
+    if (data[0] == 0x02 && data[1] == 0x01) { // Motor Start
+        printf("Received Motor Start\n");
+    } else if (data[0] == 0x02 && data[1] == 0x02) { // Motor Stop
+        printf("Received Motor Stop\n");
+    } else if (data[0] == 0x02 && data[1] == 0x03) { // Motor Param Query
+        printf("Received Query Motor %02x\n", data[2]);
+        if (data[2] == 0x00) {
+            publishMotorSetting();
+        } else {
+            publishMotorStatus();
+        }
+    }
+}
+
 esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 {
     esp_mqtt_client_handle_t client = event->client;
@@ -13,7 +45,7 @@ esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
     switch (event->event_id) {
         case MQTT_EVENT_CONNECTED://MQTT连上事件
             Serial.printf("MQTT_EVENT_CONNECTED\n");
-            msg_id = esp_mqtt_client_subscribe(client, "/iot_monitor/device", 1);
+            msg_id = esp_mqtt_client_subscribe(client, topic_rx, 1);
             // ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
             break;
         case MQTT_EVENT_DISCONNECTED://MQTT断开连接事件
@@ -38,7 +70,8 @@ esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
             for (int i = 0; i < event->data_len; i++)
                 printf("%02x ", event->data[i]);
             printf("\n");
-
+            
+            parseMqttData(event->data, event->data_len);
             break;
         case MQTT_EVENT_ERROR://MQTT错误事件
             Serial.printf("MQTT_EVENT_ERROR\n");
@@ -55,13 +88,13 @@ void mqtt_app_start(void)
     mqtt_cfg.port = 8883;
     mqtt_cfg.cert_pem = (const char *)mosquitto_org_pem_start;
 
-    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    client = esp_mqtt_client_init(&mqtt_cfg);
     esp_mqtt_client_start(client);
 }
 
 void setup() {
     Serial.begin(115200);
-    WiFi.begin("xx", "xxx");
+    WiFi.begin("JuHome", "Cc326303");
     Serial.printf("WIFI_CONNECTING\n");
     while (WiFi.status() != WL_CONNECTED) //等待网络连接成功
     {
